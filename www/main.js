0d1f7a840e77943fdfd9da03c31de744a205dbb2
Sijo <sijosaju15@gmail.com>
2025-03-09 11:09:56 +0530

    jarvis sound

--- www/main.js
+++ www/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.text').textillate({
+    $('.tlt').textillate({
         loop: true,
         sync: true,
         in: {
@@ -9,4 +9,27 @@ $(document).ready(function () {
             effect: "bounceOut",
         },
     });
+    //siri config
+   var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 640,
+    height: 200,
+    style:'ios9',
+    amplitude:'1',
+    speed:'0.30',
+    autostart:true
+  });
+  //siri message animation
+  $('.siri-message').textillate({
+    loop: true,
+    sync: true,
+    in: {
+        effect: "fadeInUp",
+        sync:true
+    },
+    out: {
+        effect: "fadeOutUp",
+        sync:true
+    },
+});
 });
